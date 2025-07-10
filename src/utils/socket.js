76bf99596e9c4cd22a0f72dffd2e0636af780322
Nitesh Prajapati <niteshkmrprajapati298@gmail.com>
2025-07-10 15:44:38 +0530

    updating socket.js base_url

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
+import { BASE_URL } from "./constants";
 
-const socket = io("http://localhost:7777", {
+const socket = io(BASE_URL, {
   withCredentials: true,
 });
 
