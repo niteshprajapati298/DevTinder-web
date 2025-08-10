4e7808208e4096cf3a7dd89d0b0361192b71540f
Nitesh Prajapati <niteshkmrprajapati298@gmail.com>
2025-08-10 18:02:55 +0530

    base_url chaned

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 //   withCredentials: true,
 // });
 
-const socket = io("http://tinderdev.xyz", {
+const socket = io("/api", {
   withCredentials: true,
   transports: ["websocket"], 
 });
