08c7d5c019b0358fcee42bf8a825be4d2a5b4a6e
Nitesh Prajapati <niteshkmrprajapati298@gmail.com>
2025-09-07 23:47:14 +0530

    BASE_URL

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://devtinder-ziqq.onrender.com", {
+const socket = io("https://devtinder-ziqq.onrender.com/", {
   withCredentials: true,
   transports: ["websocket"],
 });
